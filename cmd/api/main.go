7d3ed538a044b9d0a782098baa512081c14219cc
ShadrackAdwera <adweshshaddie@gmail.com>
2023-07-15 09:43:58 +0300

    strat task processor to poll for unprocessed tasks in queue

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -7,8 +7,11 @@ import (
 
 	"github.com/ShadrackAdwera/pg-csv/internal/rest"
 	internal "github.com/ShadrackAdwera/pg-csv/internal/sqlc"
+	"github.com/ShadrackAdwera/pg-csv/internal/workers"
+	"github.com/hibiken/asynq"
 	"github.com/jackc/pgx/v5/pgxpool"
 	"github.com/joho/godotenv"
+	zerolog "github.com/rs/zerolog/log"
 )
 
 func main() {
@@ -25,12 +28,30 @@ func main() {
 		log.Fatal("Error connecting to the DB")
 	}
 
-	store := internal.NewStore(pool)
+	redisOpts := asynq.RedisClientOpt{
+		Addr: os.Getenv("REDIS_ADDRESS"),
+	}
 
-	srv := rest.NewServer(pool, store)
+	store := internal.NewStore(pool)
+	distro := workers.NewTaskDistributor(redisOpts)
+	srv := rest.NewServer(pool, store, distro)
+
+	go runTaskProcessor(redisOpts, pool)
 
 	if err = srv.StartServer(os.Getenv("SERVER_ADDRESS")); err != nil {
 		log.Fatal(err)
 	}
 
+}
+
+func runTaskProcessor(opts asynq.RedisClientOpt, pool *pgxpool.Pool) {
+	processor := workers.NewTaskProcessor(opts, pool)
+
+	err := processor.Start()
+
+	if err != nil {
+		zerolog.Fatal().Err(err).Msg("unable to start task processor")
+	}
+
+	zerolog.Info().Msg("started task processor")
 }
